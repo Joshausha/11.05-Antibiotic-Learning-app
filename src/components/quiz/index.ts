70d32fe90dd382071ce4c5c6f1e28788f52c37e7
joshpankin <josh.pankin@gmail.com>
2026-01-07 09:55:11 -0500

    chore(04-01): add quiz components barrel file
    Enables clean imports: import { QuizExplanation } from '../components/quiz'

---
+++ src/components/quiz/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Quiz Components Barrel File
+ *
+ * Enables clean imports:
+ * import { QuizExplanation } from '../components/quiz'
+ *
+ * Additional exports will be added as components are created in subsequent plans.
+ */
+
+export { default as QuizExplanation } from './QuizExplanation';
